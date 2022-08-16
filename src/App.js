6b15aafd0850f99be14939ec5192e702aec04d13
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-08-16 15:18:32 +0100

    🛒Adding more routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Client/Home/Home";
+import MoviePage from "./components/Client/Movies/movie";
+import Login from "./components/Client/Auth/Login";
 import "./App.css";
 
 function App() {
+  const dataStorage = JSON.parse(localStorage.getItem("movie"));
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route
+            path={"/movies/"}
+            element={
+              <MoviePage
+                id={dataStorage.id}
+                Title={dataStorage.name}
+                year={dataStorage.year}
+                image={dataStorage.thumbnail}
+                length={dataStorage.length}
+                rating={dataStorage.Rating}
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
