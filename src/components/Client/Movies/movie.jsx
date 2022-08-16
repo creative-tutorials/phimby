9684d38d9ff9bcad5b9c15c1f1ef30d1c1b6b136
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-08-16 15:24:49 +0100

    Added a route to the movie page

---
+++ src/components/Client/Movies/movie.jsx
@@ -0,0 +1,47 @@
+import { Header } from "../../Client/Home/Header/Header";
+import "../../../styles/moivecss.css";
+import { useState, useRef } from "react";
+const Movie = (props) => {
+  const [stateData, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const searchInput = useRef();
+  const fetchAPIContent = async () => {
+    const inputVal = searchInput.current.value;
+    const url = await fetch(
+      `http://localhost:5000/api/movies/name/${inputVal}`
+    );
+    const data = await url.json();
+    if (data.error) {
+      setError(data.error);
+    } else {
+      setData(data);
+      console.log(data);
+      localStorage.setItem("movie", JSON.stringify(data));
+      setError(null);
+      window.location.reload(); // reload the page
+    }
+  };
+  const LoadPageTitle = () => {
+    const movie = JSON.parse(localStorage.getItem("movie"));
+    if (movie) {
+      document.title = `${movie.name}`;
+    }
+  };
+  return (
+    <div className="movie" onLoad={LoadPageTitle}>
+      <Header searchInput={searchInput} fetchAPIContent={fetchAPIContent} />
+      <div className="movie__info">
+        <h2>{props.Title}</h2>
+        <div className="items_list">
+          <p>{props.year}</p>
+          <p className="dot">{props.length}</p>
+          <p className="dot">{props.rating}</p>
+        </div>
+      </div>
+      <div className="movie__poster">
+        <img src={props.image} alt={props.Title} />
+      </div>
+    </div>
+  );
+};
+export default Movie;
