b245dd7906f0e7fcd8c2f812197c66541c05d60a
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-08-16 15:26:06 +0100

    🚚 Converted all api data to an object data

--- src/components/Client/Response/resObject.js
+++ src/components/Client/Response/resObject.js
@@ -1,12 +1,44 @@
-export function ResponseData(data) {
+export function responseData(data) {
   return [
     {
-      id: data.id,
-      name: data.name,
-      length: data.length,
-      year: data.year,
-      Rating: data.Rating,
-      thumbnail: data.thumbnail,
+      id: data[0].id,
+      name: data[0].name,
+      length: data[0].length,
+      year: data[0].year,
+      Rating: data[0].Rating,
+      thumbnail: data[0].thumbnail,
+    },
+    {
+      id: data[1].id,
+      name: data[1].name,
+      length: data[1].length,
+      year: data[1].year,
+      Rating: data[1].Rating,
+      thumbnail: data[1].thumbnail,
+    },
+    {
+      id: data[2].id,
+      name: data[2].name,
+      length: data[2].length,
+      year: data[2].year,
+      Rating: data[2].Rating,
+      thumbnail: data[2].thumbnail,
+    },
+    {
+      id: data[3].id,
+      name: data[3].name,
+      length: data[3].length,
+      year: data[3].year,
+      Rating: data[3].Rating,
+      thumbnail: data[3].thumbnail,
+    },
+    {
+      id: data[7].id,
+      name: data[7].name,
+      length: data[7].length,
+      year: data[7].year,
+      Rating: data[7].Rating,
+      thumbnail: data[7].thumbnail,
     },
   ];
 }
