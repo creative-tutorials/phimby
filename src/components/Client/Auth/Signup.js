5ad42ad2a89f89881842d13c596589e2c055005f
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-08-21 20:50:19 +0100

    😉 Singup page coming soon

---
+++ src/components/Client/Auth/Signup.js
@@ -0,0 +1,19 @@
+const SingupPage = () => {
+    const styles = {
+        color: "#FFF",
+        backgroundColor: "#000",
+        align: "center",
+        fontsize: "40px",
+    }
+  return (
+    <>
+      <h1 style={{
+        textAlign: styles.align,
+        color: styles.color,
+        backgroundColor: styles.backgroundColor,
+        fontSize: styles.fontsize
+      }}>Signup Page Coming Soon 🍟</h1>
+    </>
+  );
+};
+export default SingupPage;
